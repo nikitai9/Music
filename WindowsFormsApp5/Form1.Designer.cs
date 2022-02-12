4e97d177f03ef479d4d166bd223ee077b04f3d1c
student <DaniilSemenov999@gmail.com>
2022-02-12 13:14:05 +0200

    v 0.1

--- WindowsFormsApp5/Form1.Designer.cs
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace WindowsFormsApp5
 {
     partial class Form1
@@ -33,7 +36,53 @@ namespace WindowsFormsApp5
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
+
+            pictureBox.BackgroundImage = Image.FromFile("gif.gif");
+            pictureBox.Location = new Point(280, 0);
+            pictureBox.Size = new Size(180, 179);
+            this.Controls.Add(pictureBox);
+
+            button1.Text = "<";
+            button1.Location = new Point(220, 230);
+            button1.Size = new Size(50, 50);
+            this.Controls.Add(button1);
+
+            button2.BackgroundImage = Image.FromFile("play.png");
+            button2.Location = new Point(350, 240);
+            button2.Size = new Size(50, 28);
+            this.Controls.Add(button2);
+
+            button3.Text = ">";
+            button3.Location = new Point(480, 230);
+            button3.Size = new Size(50, 50);
+            this.Controls.Add(button3);
+
+            button4.Text = "+";
+            button4.Location = new Point(560, 350);
+            button4.Size = new Size(50, 50);
+            this.Controls.Add(button4);
+
+            button5.Text = "-";
+            button5.Location = new Point(560, 400);
+            button5.Size = new Size(50, 50);
+            this.Controls.Add(button5);
+
+            listBox.Location = new Point(225, 330);
+            listBox.Size = new Size(300, 200);
+            this.Controls.Add(listBox);
         }
+
+        Button button1 = new Button();
+        Button button2 = new Button();
+        Button button3 = new Button();
+
+        Button button4 = new Button();
+        Button button5 = new Button();
+
+        ListBox listBox = new ListBox();
+
+        PictureBox pictureBox = new PictureBox();
+
 
         #endregion
     }
