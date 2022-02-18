2dba4ed9c325397e229e85315376fd3dd903273b
Ivan_pm42@student.itstep.org <Ivan_pm42@student.itstep.org>
2022-02-18 19:01:17 +0200

    logic

--- WindowsFormsApp5/Form1.Designer.cs
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
 
 namespace WindowsFormsApp5
 {
@@ -45,21 +49,25 @@ namespace WindowsFormsApp5
             button1.Text = "<";
             button1.Location = new Point(220, 230);
             button1.Size = new Size(50, 50);
+            button1.Click += button1_Click;
             this.Controls.Add(button1);
 
             button2.BackgroundImage = Image.FromFile("play.png");
             button2.Location = new Point(350, 240);
             button2.Size = new Size(50, 28);
+            button2.Click += button2_Click;
             this.Controls.Add(button2);
 
             button3.Text = ">";
             button3.Location = new Point(480, 230);
             button3.Size = new Size(50, 50);
+            button3.Click += button3_Click;
             this.Controls.Add(button3);
 
             button4.Text = "+";
             button4.Location = new Point(560, 350);
             button4.Size = new Size(50, 50);
+            button4.Click += button4_Click;
             this.Controls.Add(button4);
 
             button5.Text = "-";
@@ -71,6 +79,59 @@ namespace WindowsFormsApp5
             listBox.Size = new Size(300, 200);
             this.Controls.Add(listBox);
         }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(music[num]);
+            simpleSound.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (num > 0)
+            {
+                num--;
+                SetMusic(num);
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (num < PictureCount)
+            {
+                num++;
+                SetMusic(num);
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+        void SetMusic(int indx = 0)
+        {
+        }
+        int PictureCount = 0;
+        int num = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "*.**|*.*|*.mp3|*.mp3|*.wav|*.wav";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    PictureCount++;
+                    num++;
+                    music.Add(dialog.FileName);
+                    listBox.Items.Add(dialog.FileName);
+                }
+            }
+        }
+
+
+        List<string> music = new List<string>();
 
         Button button1 = new Button();
         Button button2 = new Button();
